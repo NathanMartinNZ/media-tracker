9e94843e224268dcc42e97754818cbb3b6a1608f
Nate <rewq2502@gmail.com>
2023-01-05 21:42:02 +1300

    Changed movie to static prop

--- src/pages/m/[mediaId].tsx
+++ src/pages/m/[mediaId].tsx
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router"
 import { api } from "../../utils/api";
+import { prisma } from "../../server/db"
 
-export const mediaPage = () => {
-  const router = useRouter()
-  const mediaId = parseInt(router.query.mediaId as string, 10)
-  if(!mediaId) { return }
-  const movie = api.movies.getMovieById.useQuery(mediaId)
+export const mediaPage = ({ movie }:{ movie:any }) => {
+  // const router = useRouter()
+  // const mediaId = parseInt(router.query.mediaId as string, 10)
+  // if(!mediaId) { return }
+  // const movie = api.movies.getMovieById.useQuery(mediaId)
 
   return (
     <div>
@@ -13,5 +14,25 @@ export const mediaPage = () => {
     </div>
   )
 };
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true
+  }
+}
+
+export async function getStaticProps(ctx:any) {
+  const movieId = parseInt(ctx.params.mediaId)
+  const movie = await prisma.movie.findUnique({
+    where: {
+      id: movieId
+    }
+  })
+
+  return {
+    props: { movie }
+  }
+}
 
 export default mediaPage;
