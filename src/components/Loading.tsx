982510f82b04fb8d2903942cb98cb65103af60b4
Nate <rewq2502@gmail.com>
2023-06-17 18:13:40 +1200

    fix: removed background from Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,5 @@
 const Loading = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center bg-gradient-to-r from-[#2e026d] to-[#15162c]"></main>
-  );
+  return <main className="flex min-h-screen flex-col items-center"></main>;
 };
 
 export default Loading;
